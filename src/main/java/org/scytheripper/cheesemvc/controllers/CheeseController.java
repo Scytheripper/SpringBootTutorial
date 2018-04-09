1899219a77ade27568862eaffd768014f6551f3f
Sanjay Shivraj <Scytheripper@yandex.com>
2018-04-08 21:21:06 -0400

    Initial commit

---
+++ src/main/java/org/scytheripper/cheesemvc/controllers/CheeseController.java
@@ -0,0 +1,36 @@
+package org.scytheripper.cheesemvc.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+
+@Controller
+@RequestMapping(value="cheese")
+public class CheeseController {
+    static ArrayList<String> cheeses = new ArrayList<>();
+
+    // Reuquest path: /cheese
+    @RequestMapping(value="")
+    public String index(Model model){
+        model.addAttribute("cheeses", cheeses);
+        model.addAttribute("title", "My Cheeses");
+        return "cheese/index";
+    }
+
+    @RequestMapping(value="add", method = RequestMethod.GET)
+    public String displayAddCheeseForm(Model model){
+        model.addAttribute("title", "Add Cheese");
+        return "cheese/add";
+    }
+
+    @RequestMapping(value="add", method = RequestMethod.POST)
+    public String processAddCheeseForm(@RequestParam String cheeseName){
+        cheeses.add(cheeseName);
+        //redirects to /cheese due to the RequestMapping on the controller
+        return "redirect:";
+    }
+}
